1f5397dc99e2f9d48e5555ff4bbebc51a35523ee
ShilpaHotagi <shilpahotagi137@gmail.com>
2022-04-05 08:54:51 +0530

    commit

--- TypeScript/Activity-1/README.md
+++ TypeScript/Activity-1/README.md
@@ -2,4 +2,6 @@
 
 ![Screenshot (671)](https://user-images.githubusercontent.com/98829965/161313485-bb3a0b0d-cf8d-407d-8697-0f641e6a9bda.png)
 
+![Screenshot (684)](https://user-images.githubusercontent.com/98829965/161672601-75fc5d77-bc47-43f5-b7ca-c217944e2fee.png)
+
 ![Screenshot (672)](https://user-images.githubusercontent.com/98829965/161313500-76b695b7-28cb-423a-ad5c-bc5c4f3f4e55.png)
--- TypeScript/Activity-4/Abstract.ts
+++ TypeScript/Activity-4/Abstract.ts
@@ -1,37 +1,171 @@
-export abstract class Match {
-    
-    private currentScore:number;
-    private currentOver: number;
-    private target:number; 
-  
-    constructor(currentScore: number,currentOver: number, target: number) {
-      this.currentScore = currentScore;
-      this.currentOver = currentOver;
-      this.target = target;
+abstract class Match
+{
+
+	private currentScore:number;
+	private currentOver:number;
+	private target:number; 
+	
+ constructor(currentScore:number,currentOver:number,target:number) 
+    {
+        this.currentScore = currentScore;
+        this.currentOver = currentOver;
+        this.target = target;
+		
+		// TODO Auto-generated constructor stub
+	}
+
+public getCurrentScore():number 
+    {
+	  return this.currentScore;
     }
 
-public getCurrentScore() 
-{
-	return this.currentScore;
+public getCurrentOver():number 
+    {
+	  return this.currentOver;
+    }
+
+public getTarget():number 
+    {
+	  return this.target;
+    }
+
+ abstract calculateRunRate():number;
+ abstract calculateBalls():number;
+
+ abstract display():void;
+
 }
 
-public getCurrentOver() 
+
+ class ODIMatch extends Match 
 {
-	return this.currentOver;
+
+	
+
+
+	override calculateRunRate():number 
+    {
+		// TODO Auto-generated method stub
+		return ((this.getTarget()-this.getCurrentScore())/(50-this.getCurrentOver()));
+		
+	}
+
+	override calculateBalls():number 
+    {
+		// TODO Auto-generated method stub
+		
+		return 6*(50-this.getCurrentOver());
+	}
+	
+	public display():void
+	{
+		console.log("Need "+(this.getTarget()-this.getCurrentScore())+" runs in "+<number>this.calculateBalls() + " balls");
+		console.log("Required Runrate: "+this.calculateRunRate());
+		
+	}
+    
 }
 
-public getTarget() 
+ class T20Match extends Match
 {
-	return this.target;
+
+	public T20Match()
+    {
+		// TODO Auto-generated constructor stub
+	}
+
+	override calculateRunRate():number 
+    {
+		return ((this.getTarget()-this.getCurrentScore())/(20-this.getCurrentOver()));
+	}
+
+	override calculateBalls():number
+    {
+		// TODO Auto-generated method stub
+		return 6*(20-this.getCurrentOver());
+	}
+	public display():void 
+	{
+		console.log("Need "+(this.getTarget()-this.getCurrentScore())+" runs in "+<number>this.calculateBalls() + " balls");
+		console.log("Required Runrate: "+this.calculateRunRate());
+		
+	}
+
 }
 
-public getcalculateRunRate():number
+ class TestMatch extends Match
 {
-    return 0;
+
+	public TestMatch() {
+		
+		// TODO Auto-generated constructor stub
+	}
+
+	override calculateRunRate():number
+    {
+		return ((this.getTarget()-this.getCurrentScore())/(90-this.getCurrentOver()));
+	}
+	override calculateBalls():number
+    {
+		// TODO Auto-generated method stub
+		return 6*(90-this.getCurrentOver());
+	}
+	public display():void
+	{
+		console.log("Need "+(this.getTarget()-this.getCurrentScore())+" runs in "+<number>this.calculateBalls() + " balls");
+		console.log("Required Runrate: "+this.calculateRunRate());
+		
+	}
 }
- 
-public getcalculateBalls():number
-{
-    return 0;
-}
+
+console.log("Enter The Match Format");
+console.log("1.ODI");
+console.log("2.T20Match");
+console.log("3.TestMatch");
+
+      // TODO Auto-generated method stub
+	
+		
+		console.log("Enter the Current Score");
+		
+		let cScore:number;		
+		
+		console.log("Enter the Current Over");
+		
+		let cOver:number;
+
+		console.log("Enter the Target Score");
+		
+		let tScore:number;
+		
+		console.log("Requirements:");
+		
+		switch(c)
+		{
+		case 1: ODIMatch o1 =new ODIMatch();
+				o1.setCurrentScore(cScore);
+				o1.setCurrentOver(cOver);
+				o1.setTarget(tScore);
+				o1.display();
+			break;
+			
+		case 2: T20Match tt1 = new T20Match();
+				tt1.setCurrentScore(cScore);
+				tt1.setCurrentOver(cOver);
+				tt1.setTarget(tScore);
+				tt1.display();
+			break;
+		
+		case 3: TestMatch t1 = new TestMatch();
+				t1.setCurrentScore(cScore);
+				t1.setCurrentOver(cOver);
+				t1.setTarget(tScore);
+				t1.display();
+			break;
+			
+		default: System.exit(0);
+		}
+		
+		scanner.close();
+
 }
